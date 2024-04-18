a1ec17e867ac53ea1547339ae854a5b2a852cbf9
cristianfbolanos@unicauca.edu.co <cristianfbolanosfelip@LAPTOP-D4DRQ08Q MINGW64 ~/aplicacion (master)$ git initReinitialized existing Git repository in C:/Users/felip/aplicacion/.git/felip@LAPTOP-D4DRQ08Q MINGW64 ~/aplicacion (master)$ git add.git: 'add.' is not a git command. See 'git --help'.The most similar command is        addfelip@LAPTOP-D4DRQ08Q MINGW64 ~/aplicacion (master)$ git add .felip@LAPTOP-D4DRQ08Q MINGW64 ~/aplicacion (master)$felip@LAPTOP-D4DRQ08Q MINGW64 ~/aplicacion (master)$ git commit -m primer commiterror: pathspec 'commit' did not match any file(s) known to gitfelip@LAPTOP-D4DRQ08Q MINGW64 ~/aplicacion (master)$ git commit -m primer>
2024-04-17 23:04:36 -0500

    se agrego el componente interfaz principal y la biblioteca de boots trap

---
+++ src/app/interfaz-principal/interfaz-principal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-interfaz-principal',
+  standalone: true,
+  imports: [],
+  templateUrl: './interfaz-principal.component.html',
+  styleUrl: './interfaz-principal.component.css'
+})
+export class InterfazPrincipalComponent {
+
+}
