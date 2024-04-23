930bb80fb2933467b7f2f7559ed4c2f62f0b5dd4
CollazosC2006 <cdcollazos@unicauca.edu.co>
2024-04-23 01:10:23 -0500

    j

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { InterfazPrincipalComponent } from "./interfaz-principal/interfaz-principal.component";
 import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, InterfazPrincipalComponent,LoginComponent]
+    imports: [RouterOutlet, InterfazPrincipalComponent,LoginComponent,FormsModule]
 })
 export class AppComponent {
   title = 'aplicacion';
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,30 +1,34 @@
 import { Component,OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule ,CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent implements OnInit{
+export class LoginComponent{
     
   
     userId: string='';
     password: string='';
-    loginError: string = '';
+    loginError: boolean = false;
+    errorLogin: string='';
 
     constructor(){}
 
-    onSubmit(): void {
-      // Aquí puedes agregar la lógica para manejar el envío del formulario
+    onSubmit(event: Event): void {
+      event.preventDefault();
+        // Aquí puedes agregar la lógica para manejar el envío del formulario
       // Por ahora, solo estableceremos el mensaje de error para demostración
-      this.loginError = "hecho";
+      this.loginError = true;
+      this.errorLogin="Credenciales incorrectos. Por favor, inténtalo de nuevo.";
+      
+      
     
     }
-    ngOnInit(): void {
-      
-    }
-}
+  }
+    
