6cecb797a39f3bb9d8ce6bd154f39ab42271da41
cristianfbolanos@unicauca.edu.co <cristianfbolanosfelip@LAPTOP-D4DRQ08Q MINGW64 ~/aplicacion (master)$ git initReinitialized existing Git repository in C:/Users/felip/aplicacion/.git/felip@LAPTOP-D4DRQ08Q MINGW64 ~/aplicacion (master)$ git add.git: 'add.' is not a git command. See 'git --help'.The most similar command is        addfelip@LAPTOP-D4DRQ08Q MINGW64 ~/aplicacion (master)$ git add .felip@LAPTOP-D4DRQ08Q MINGW64 ~/aplicacion (master)$felip@LAPTOP-D4DRQ08Q MINGW64 ~/aplicacion (master)$ git commit -m primer commiterror: pathspec 'commit' did not match any file(s) known to gitfelip@LAPTOP-D4DRQ08Q MINGW64 ~/aplicacion (master)$ git commit -m primer>
2024-04-24 10:07:29 -0500

    se agrego rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { ReadAdminLocalComponent } from './read-admin-local/read-admin-local.com
 import { UpdateAdminLocalComponent } from './update-admin-local/update-admin-local.component';
 import { DeleteAdminLocalComponent } from './delete-admin-local/delete-admin-local.component';
 import { InterfazPrincipalComponent } from './interfaz-principal/interfaz-principal.component';
+import { LucesComponent } from './luces/luces.component';
+import { AlarmaComponent } from './alarma/alarma.component';
 
 export const routes: Routes = [
 { path: '', redirectTo: '/crear', pathMatch: 'full' },
@@ -12,7 +14,9 @@ export const routes: Routes = [
   { path: 'update-admin-local', component: UpdateAdminLocalComponent },
   { path: 'delete-admin-local', component: DeleteAdminLocalComponent },
    
-  { path: 'interfaz-principal', component: InterfazPrincipalComponent }
+  { path: 'interfaz-principal', component: InterfazPrincipalComponent },
+  { path: 'luces', component: LucesComponent },
+  { path: 'alarma', component: AlarmaComponent }
 
 
 ];
