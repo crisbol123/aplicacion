28285a48054733ca196e37b9d6c7b4842c108587
cristianfbolanos@unicauca.edu.co <cristianfbolanosfelip@LAPTOP-D4DRQ08Q MINGW64 ~/aplicacion (master)$ git initReinitialized existing Git repository in C:/Users/felip/aplicacion/.git/felip@LAPTOP-D4DRQ08Q MINGW64 ~/aplicacion (master)$ git add.git: 'add.' is not a git command. See 'git --help'.The most similar command is        addfelip@LAPTOP-D4DRQ08Q MINGW64 ~/aplicacion (master)$ git add .felip@LAPTOP-D4DRQ08Q MINGW64 ~/aplicacion (master)$felip@LAPTOP-D4DRQ08Q MINGW64 ~/aplicacion (master)$ git commit -m primer commiterror: pathspec 'commit' did not match any file(s) known to gitfelip@LAPTOP-D4DRQ08Q MINGW64 ~/aplicacion (master)$ git commit -m primer>
2024-04-25 00:13:17 -0500

    configure la parte de luces, añadiendo el post y el get para las 3 luces

--- src/app/luces/luces.component.ts
+++ src/app/luces/luces.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import {MatButtonModule} from '@angular/material/button';
 @Component({
   selector: 'app-luces',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './luces.component.html',
   styleUrl: './luces.component.css'
 })
+
 export class LucesComponent implements OnInit {
-  luces=[];
+  
+  luces:Luz[]=[];
   
   source1 = '';
   source2 = '';
@@ -19,50 +22,30 @@ export class LucesComponent implements OnInit {
   
   constructor(private http: HttpClient) {}
 
+  cambiarEstado(luzo:Luz){
+      console.log(luzo);
+      luzo.estado=!luzo.estado;
+      const data = { estado: luzo.estado, id_bombillo: luzo.id_bombillo }; // Datos que quieres enviar en la solicitud
+    this.http.post('http://localhost:8081/luces/actualizarEstado', data).subscribe(response => {
+      console.log(response);
+  });
+  }
 
 ngOnInit(): void {
-  /*const data = { estado: 'valor1', id_bombillo: 'valor2' }; // Datos que quieres enviar en la solicitud
-    this.http.post('http://localhost:8081/luces/actualizarEstado', data).subscribe(response => {
-      console.log(response);*/
+  
 
   this.http.get<any>('http://localhost:8081/luces/recibirEstado').subscribe(data => {
     console.log(data); 
-data.data_rta.forEach((element: { id_bombillo: any; estado: string; })  => {
-  switch(element.id_bombillo){
-    case 1:
-      if(element.estado == '1'){
-        this.source1 ='https://png.pngtree.com/png-vector/20201208/ourlarge/pngtree-flat-light-bulb-turn-off-isolated-vector-png-image_2531328.jpg';
-         }else{
-  this.source1 ='https://www.shutterstock.com/image-vector/light-bulb-full-ideas-creative-260nw-1531484285.jpg';
-         }
-      break;
-    case 2:
-      if(element.estado == '1'){
-        this.source2 ='https://png.pngtree.com/png-vector/20201208/ourlarge/pngtree-flat-light-bulb-turn-off-isolated-vector-png-image_2531328.jpg';
-         }else{
-  this.source2 ='https://www.shutterstock.com/image-vector/light-bulb-full-ideas-creative-260nw-1531484285.jpg';
-         }
-      break;
-    case 3:
-      if(element.estado == '1'){
-        this.source3 ='https://png.pngtree.com/png-vector/20201208/ourlarge/pngtree-flat-light-bulb-turn-off-isolated-vector-png-image_2531328.jpg';
-         }else{
-  this.source3 ='https://www.shutterstock.com/image-vector/light-bulb-full-ideas-creative-260nw-1531484285.jpg';
-         }
-      break;
-    default:
-      console.log("El valor del id_bombillo no existe");
-      break;
-  
-  }
+    this.luces= data;
 });
-
-
-   
   
-  }
-)
 }
+}
+
+export class Luz{
+  id_bombillo:any;
+  estado: any;
+  fecha_estado:any;
 }
 
 
