c0282299cf54f6df8311aaaad95bcec3615e97c1
CollazosC2006 <cdcollazos@unicauca.edu.co>
2024-05-03 15:59:48 -0500

    as

--- backend/connection.js
+++ backend/connection.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2/promise');
 
  
 const pool = mysql.createPool(
-    {host: 'localhost',
-user: 'root',
-password: 'root',
-database: 'prueba2',
+{host: 'localhost',
+user: 'admin',
+password: '12345',
+database: 'smart home',
 waitForConnections: true,
 connectionLimit: 10,
 queueLimit: 0,}
