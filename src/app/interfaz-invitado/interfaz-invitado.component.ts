45046e217a48a245e1574534c74f433cbf04e267
cristianfbolanos@unicauca.edu.co <cristianfbolanosfelip@LAPTOP-D4DRQ08Q MINGW64 ~/aplicacion (master)$ git initReinitialized existing Git repository in C:/Users/felip/aplicacion/.git/felip@LAPTOP-D4DRQ08Q MINGW64 ~/aplicacion (master)$ git add.git: 'add.' is not a git command. See 'git --help'.The most similar command is        addfelip@LAPTOP-D4DRQ08Q MINGW64 ~/aplicacion (master)$ git add .felip@LAPTOP-D4DRQ08Q MINGW64 ~/aplicacion (master)$felip@LAPTOP-D4DRQ08Q MINGW64 ~/aplicacion (master)$ git commit -m primer commiterror: pathspec 'commit' did not match any file(s) known to gitfelip@LAPTOP-D4DRQ08Q MINGW64 ~/aplicacion (master)$ git commit -m primer>
2024-05-10 21:23:31 -0500

    se creo interfaz invitado

---
+++ src/app/interfaz-invitado/interfaz-invitado.component.ts
@@ -0,0 +1,41 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { LucesComponent } from "../luces/luces.component";
+import { BarraAdminLocalComponent } from "../barra-admin-local/barra-admin-local.component";
+import {MatButtonModule} from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { AlarmaComponent } from "../alarma/alarma.component";
+import { PuertasComponent } from '../puertas/puertas.component';
+
+@Component({
+    selector: 'app-interfaz-invitado',
+    standalone: true,
+    templateUrl: './interfaz-invitado.component.html',
+    styleUrl: './interfaz-invitado.component.css',
+    imports: [CommonModule, FormsModule, LucesComponent, BarraAdminLocalComponent, MatButtonModule, MatIconModule, AlarmaComponent,PuertasComponent]
+})
+export class InterfazPrincipalComponent {
+  
+mostrarComponente:boolean = false;
+mostrarComponente2:boolean = false;
+mostrarComponente3:boolean = false;
+mostrarComponente4:boolean = false;
+cambiarVisibilidad(){
+
+  this.mostrarComponente=!this.mostrarComponente;
+}
+
+cambiarVisibilidad2(){
+
+  this.mostrarComponente2=!this.mostrarComponente2;
+}
+cambiarVisibilidad3(){
+
+  this.mostrarComponente3=!this.mostrarComponente3;
+}
+cambiarVisibilidad4(){
+
+  this.mostrarComponente4=!this.mostrarComponente4;
+}
+}
